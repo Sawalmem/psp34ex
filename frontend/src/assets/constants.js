46277d6263a0d080265ecd77527768f09805821f
Rafat Hussain <rafat.hsn@gmail.com>
2022-07-10 10:00:19 +0530

    shibuya rpc

--- frontend/src/assets/constants.js
+++ frontend/src/assets/constants.js
@@ -1,2 +1,4 @@
-export const PSP34EX_ADDRESS = "5DskzVevUBQVicYUgS1zyUKa1WH6ZFDPjCewCuo8WeofuhEy";
-export const RPC_URL = "ws://127.0.0.1:9944";
+export const PSP34EX_ADDRESS = "b8RR8GDZjoFHjHDR3dnSvQGXGRCX86WaxssUC9yiEUkHE6d";
+export const RPC_URL = "wss://rpc.shibuya.astar.network";
+export const PSP34EX_ADDRESS_SHIBUYA = "b8RR8GDZjoFHjHDR3dnSvQGXGRCX86WaxssUC9yiEUkHE6d";
+export const RPC_URL_SHIBUYA = "wss://rpc.shibuya.astar.network";
