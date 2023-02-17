6c254548504e0b35f846f7729f6c9e9158ca16aa
Ayush Arora <ayusharora1778@gmail.com>
2023-02-17 15:45:58 +0530

    feat: integrating contract WIP

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,111 +1,152 @@
-import React, {useState,useEffect, useCallback} from 'react'; 
+import React, { useState, useEffect, useCallback } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
-import {
-  web3Accounts, 
-  web3Enable, 
-  web3AccountsSubscribe, 
-  web3FromSource
-} from '@polkadot/extension-dapp';
+  web3Accounts,
+  web3Enable,
+  web3AccountsSubscribe,
+  web3FromSource,
+} from "@polkadot/extension-dapp";
 import { ApiPromise, WsProvider } from "@polkadot/api";
-import { ContractPromise } from "@polkadot/api-contract";
-import psp34ABI from "./contracts/psp34ex.json";
-import {PSP34EX_ADDRESS,RPC_URL} from "./assets/constants";
+import { ContractPromise, Abi } from "@polkadot/api-contract";
+import {
+  MARKETPLACE_ADDRESS_ROCOCO,
+  RPC_URL_ROCOCO,
+  TOKEN_ADDRESS_ROCOCO,
+} from "./assets/constants";
+import TokenAbiData from "./contracts/token";
+import MarketplaceAbiData from "./contracts/marketplace";
 
-
-import Toolbar from './components/Toolbar';
-import Home from './containers/Home';
-import Mint from './containers/Mint.js';
-import Dashboard from './containers/Dashboard.js';
-
+import Toolbar from "./components/Toolbar";
+import Home from "./containers/Home";
+import Mint from "./containers/Mint.js";
+import Dashboard from "./containers/Dashboard.js";
+import List from "./containers/List.js";
 
 function App() {
-  const [allAccounts,setAllAccounts] = useState();
+  const [allAccounts, setAllAccounts] = useState();
   const [activeAccount, setActiveAccount] = useState(null);
-  const [nftContract,setNftContract] = useState(null);
-  const [signer,setSigner] = useState(null);
+  const [tokenContract, setTokenContract] = useState(null);
+  const [marketplaceContract, setMarketplaceContract] = useState(null);
+  const [signer, setSigner] = useState(null);
+  const [apiProimse, setApiProimse] = useState(null);
 
-  //console.log(allAccounts);
-  //console.log("active: ",activeAccount);
-
-  const walletInit = useCallback ( async () => {
-    const allInjected = await web3Enable('psp34ex');
+  const walletInit = useCallback(async () => {
+    const allInjected = await web3Enable("psp34ex");
 
     if (allInjected.length === 0) {
-        console.log("No extension installed");
-        return;
+      console.log("No extension installed");
+      return;
     }
     const temp = await web3Accounts();
     setAllAccounts(temp);
 
-    let unsubscribe; 
+    let unsubscribe;
 
-    unsubscribe = await web3AccountsSubscribe(( allAccounts ) => { 
-        allAccounts.map(( account ) => {
-            console.log("Subscribe : ",account.address);
-        })
-     });
+    unsubscribe = await web3AccountsSubscribe((allAccounts) => {
+      allAccounts.map((account) => {
+        console.log("Subscribe : ", account.address);
+      });
+    });
 
-     unsubscribe && unsubscribe();
+    unsubscribe && unsubscribe();
   });
 
   useEffect(() => {
     walletInit();
-  },[])
+  }, []);
 
   useEffect(() => {
     if (allAccounts && !activeAccount) {
       setActiveAccount(allAccounts[0]);
     }
-  },[allAccounts])
+  }, [allAccounts]);
 
   const setUp = async () => {
     if (activeAccount) {
-      const wsProvider = new WsProvider(RPC_URL);
+      const wsProvider = new WsProvider(RPC_URL_ROCOCO);
       const api = await ApiPromise.create({ provider: wsProvider });
-      const contract2 = new ContractPromise(api, psp34ABI, PSP34EX_ADDRESS);
-      setNftContract(contract2);
-      console.log("activeAccount ::::",activeAccount);
-      const accountSigner = await web3FromSource(activeAccount.meta.source).then(
-        (res) => res.signer
+      setApiProimse(api);
+      await api.isReady;
+
+      const tokenABI = new Abi(TokenAbiData, api.registry.getChainProperties());
+      const tokenContract = new ContractPromise(
+        api,
+        tokenABI,
+        TOKEN_ADDRESS_ROCOCO
       );
+
+      const marketplaceABI = new Abi(
+        MarketplaceAbiData,
+        api.registry.getChainProperties()
+      );
+      const marketplaceContract = new ContractPromise(
+        api,
+        marketplaceABI,
+        MARKETPLACE_ADDRESS_ROCOCO
+      );
+      setTokenContract(tokenContract);
+      setMarketplaceContract(marketplaceContract);
+      const accountSigner = await web3FromSource(
+        activeAccount.meta.source
+      ).then((res) => res.signer);
       setSigner(accountSigner);
     }
-  }
+  };
 
   useEffect(() => {
     setUp();
-  },[activeAccount])
+  }, [activeAccount]);
 
   const onHandleSelect = (e) => {
     e.preventDefault();
-    console.log("ETAR",e.target.value);
     setActiveAccount(allAccounts[e.target.value]);
-  }
+  };
 
   return (
     <BrowserRouter>
       <Toolbar />
       <Routes>
-        <Route path="/" element={<Home 
-        activeAccount={activeAccount} 
-        allAccounts={allAccounts}
-        onHandleSelect={onHandleSelect}
-        />}
-         />
-        <Route path="/mint" element={<Mint 
-        activeAccount={activeAccount} 
-        signer={signer}
-        nftContract={nftContract}
-        />} /> 
-        <Route path="/dashboard" element={<Dashboard 
-        activeAccount={activeAccount} 
-        signer={signer}
-        nftContract={nftContract}
-        />} /> 
+        <Route
+          path="/"
+          element={
+            <Home
+              activeAccount={activeAccount}
+              allAccounts={allAccounts}
+              onHandleSelect={onHandleSelect}
+            />
+          }
+        />
+        <Route
+          path="/mint"
+          element={
+            <Mint
+              activeAccount={activeAccount}
+              signer={signer}
+              nftContract={tokenContract}
+            />
+          }
+        />
+        <Route
+          path="/dashboard"
+          element={
+            <Dashboard
+              activeAccount={activeAccount}
+              signer={signer}
+              nftContract={marketplaceContract}
+            />
+          }
+        />
+        <Route
+          path="/list"
+          element={
+            <List
+              activeAccount={activeAccount}
+              signer={signer}
+              nftContract={tokenContract}
+              apiPromise={apiProimse}
+            />
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
--- frontend/src/containers/Mint.js
+++ frontend/src/containers/Mint.js
@@ -1,133 +1,239 @@
-import React, {useState, useCallback} from "react";
-import {Button, Container, Row, Col, Form} from "react-bootstrap";
-import { create as ipfsHttpClient } from 'ipfs-http-client';
-import {useNavigate} from 'react-router-dom';
+import React, { useState, useCallback, useEffect } from "react";
+import { Button, Container, Row, Col, Form } from "react-bootstrap";
+import { create as ipfsHttpClient } from "ipfs-http-client";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
-const client = ipfsHttpClient('https://ipfs.infura.io:5001/api/v0');
-
+const client = ipfsHttpClient("https://ipfs.infura.io:5001/api/v0");
+const proofSize = 131072;
+const refTime = 6219235328;
+const storageDepositLimit = null;
 
 const Mint = (props) => {
-    const [mintDisable,setMintDisable] = useState(false);
-    const [formState,setFormState] = useState({name: '',description: ''});
-    const [nftURL, setNftURL] = useState(null);
-    const [displayMessage, setDisplayMessage] = useState(null);
+  const [mintDisable, setMintDisable] = useState(false);
+  const [formState, setFormState] = useState({ name: "", description: "" });
+  const [nftURL, setNftURL] = useState(null);
+  const [displayMessage, setDisplayMessage] = useState(null);
+  const [files, setFiles] = useState([]);
 
-    const checkErrorsAndUploadToIPFS = async () => {
-        const {name, description} = formState;
+  const checkErrorsAndUploadToIPFS = async () => {
+    const { name, description } = formState;
 
-        if ( !name || !description ) {
-            alert("Name and Description fields are necessary");
-            return;
-        }
-
-
-        if(!nftURL) {
-            alert("Your NFT didn't upload to IPFS.");
-            return;
-        }
-
-        const tokenURI = JSON.stringify({
-            name, description, image: nftURL
-        });
-
-        try {
-            const added = await client.add(tokenURI);
-            //const url = `https://ipfs.infura.io/ipfs/${added.path}`;
-            return added.path;
-        } catch (error) {
-            console.log(error);
-        }
-
+    if (!name || !description) {
+      alert("Name and Description fields are necessary");
+      return;
     }
 
-    const onHandleMint = async (e) => {
+    if (!nftURL) {
+      alert("Your NFT didn't upload to IPFS.");
+      return;
+    }
+
+    const tokenURI = JSON.stringify({
+      name,
+      description,
+      image: nftURL,
+    });
+
+    try {
+      const added = await client.add(tokenURI);
+      //const url = `https://ipfs.infura.io/ipfs/${added.path}`;
+      return added.path;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const sendFileToIPFS = async (e) => {
+    if (files) {
+      try {
+        const formData = new FormData();
+        formData.append("file", files);
+
+        console.log({ formData });
+
+        console.log({ REACT_APP_PINATA_JWT: process.env.REACT_APP_PINATA_JWT });
+
+        const resFile = await axios({
+          method: "post",
+          url: "https://api.pinata.cloud/pinning/pinFileToIPFS",
+          data: formData,
+          headers: {
+            Authorization: `Bearer ${process.env.REACT_APP_PINATA_JWT}`,
+            "Content-Type": "multipart/form-data",
+          },
+        });
+
+        const ImgHash = `ipfs://${resFile.data.IpfsHash}`;
+        console.log(ImgHash);
+        //Take a look at your Pinata Pinned section, you will see a new file added to you list.
         setMintDisable(true);
-        const uri = await checkErrorsAndUploadToIPFS();
-        console.log(uri);
 
         const gasLimit = -1;
 
-        let item = await props.nftContract.query["psp34::collectionId"]
-        (props.activeAccount.address,{gasLimit});
+        const { gasRequired, result, output } =
+          await props.nftContract.query.get(props.activeAccount.address, {
+            gasLimit,
+            storageDepositLimit,
+          });
 
-        console.log(item);
+        console.log({ gasRequired, result, output });
 
-        if (uri) {
-            await props.nftContract.tx.mint({gasLimit},uri)
-            .signAndSend(props.activeAccount.address,{signer: props.signer}, result => {
-                console.log('Transaction status:', result.status.type);
+        let item = await props.nftContract.query["psp34::collectionId"](
+          props.activeAccount.address,
+          { gasLimit }
+        );
+
+        console.log({ item });
+
+        if (ImgHash) {
+          await props.nftContract.tx
+            .mint({ gasLimit }, ImgHash)
+            .signAndSend(
+              props.activeAccount.address,
+              { signer: props.signer },
+              (result) => {
+                console.log("Transaction status:", result.status.type);
                 setDisplayMessage(result.status.type);
                 if (result.status.isInBlock) {
-                    console.log('Included at block hash', result.status.asInBlock.toHex());
-                    setDisplayMessage(result.status.type);
-                } 
-                
-                if (result.status.isFinalized) {
-                    console.log('Finalized block hash', result.status.asFinalized.toHex());
-                    setDisplayMessage(result.status.type);
-
-                    //setDisplayMessage(result.status.asFinalized.toHex());
-                    process.exit(0)
+                  console.log(
+                    "Included at block hash",
+                    result.status.asInBlock.toHex()
+                  );
+                  setDisplayMessage(result.status.type);
                 }
-            });
-            }
 
-        //await item.wait();
-        setMintDisable(false);
-        console.log(item);
+                if (result.status.isFinalized) {
+                  console.log(
+                    "Finalized block hash",
+                    result.status.asFinalized.toHex()
+                  );
+                  setDisplayMessage(result.status.type);
+
+                  //setDisplayMessage(result.status.asFinalized.toHex());
+                  process.exit(0);
+                }
+              }
+            );
+        }
+      } catch (error) {
+        console.log("Error sending File to IPFS: ");
+        console.log(error);
+      }
     }
+  };
 
+  const onHandleMint = async (e) => {
+    setMintDisable(true);
+    const uri = await checkErrorsAndUploadToIPFS();
+    console.log(uri);
 
-    const onHandleFileChange = async (e) => {
-        const file = e.target.files[0];
-        try {
-        const added = await client.add(
-            file,
-            {
-            progress: (prog) => console.log(`received: ${prog}`)
+    const gasLimit = -1;
+
+    let item = await props.nftContract.query["psp34::collectionId"](
+      props.activeAccount.address,
+      { gasLimit }
+    );
+
+    console.log(item);
+
+    if (uri) {
+      await props.nftContract.tx
+        .mint({ gasLimit }, uri)
+        .signAndSend(
+          props.activeAccount.address,
+          { signer: props.signer },
+          (result) => {
+            console.log("Transaction status:", result.status.type);
+            setDisplayMessage(result.status.type);
+            if (result.status.isInBlock) {
+              console.log(
+                "Included at block hash",
+                result.status.asInBlock.toHex()
+              );
+              setDisplayMessage(result.status.type);
             }
+
+            if (result.status.isFinalized) {
+              console.log(
+                "Finalized block hash",
+                result.status.asFinalized.toHex()
+              );
+              setDisplayMessage(result.status.type);
+
+              //setDisplayMessage(result.status.asFinalized.toHex());
+              process.exit(0);
+            }
+          }
         );
-        console.log(added,added.path);
-        const url = `https://ipfs.infura.io/ipfs/${added.path}`;
-        setNftURL(url);
-        } catch (error) {
-        console.log('Error uploading file: ', error);
-        }  
     }
 
-    return (
-        <Container>
-            <Row>
-                <Col></Col>
-                <Col xs="6">
-                <Form>
-                <Form.Group className="mb-3" controlId="mintForm.ControlInput1">
-                    <Form.Label>Name</Form.Label>
-                    <Form.Control type="text" placeholder="NFT name"
-                    onChange={e => setFormState({ ...formState, name: e.target.value })} />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="mintForm.ControlTextarea1">
-                    <Form.Label>Description</Form.Label>
-                    <Form.Control as="textarea" rows={4} 
-                    onChange={e => setFormState({ ...formState, description: e.target.value })}/>
-                </Form.Group>
-                <Form.Group controlId="mintForm.File" className="mb-3">
-                    <Form.Label>Upload Image File</Form.Label>
-                    <Form.Control type="file" onChange={onHandleFileChange} />
-                </Form.Group>
-                <Button variant="danger" onClick={onHandleMint}
-                 disabled={mintDisable}>Mint</Button>
-                 <Form.Group>
-                    <Form.Label>
-                        {displayMessage}
-                    </Form.Label>
-                 </Form.Group>
-                </Form>
-                </Col>
-                <Col></Col>
-            </Row>
-        </Container>
-    )
-}
+    //await item.wait();
+    setMintDisable(false);
+    console.log(item);
+  };
+
+  const onHandleFileChange = async (e) => {
+    const file = e.target.files[0];
+    setFiles(file);
+    // try {
+    //   const added = await client.add(file, {
+    //     progress: (prog) => console.log(`received: ${prog}`),
+    //   });
+    //   console.log(added, added.path);
+    //   const url = `https://ipfs.infura.io/ipfs/${added.path}`;
+    //   setNftURL(url);
+    // } catch (error) {
+    //   console.log("Error uploading file: ", error);
+    // }
+  };
+
+  return (
+    <Container>
+      <Row>
+        <Col></Col>
+        <Col xs="6">
+          <Form>
+            <Form.Group className="mb-3" controlId="mintForm.ControlInput1">
+              <Form.Label>Name</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="NFT name"
+                onChange={(e) =>
+                  setFormState({ ...formState, name: e.target.value })
+                }
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="mintForm.ControlTextarea1">
+              <Form.Label>Description</Form.Label>
+              <Form.Control
+                as="textarea"
+                rows={4}
+                onChange={(e) =>
+                  setFormState({ ...formState, description: e.target.value })
+                }
+              />
+            </Form.Group>
+            <Form.Group controlId="mintForm.File" className="mb-3">
+              <Form.Label>Upload Image File</Form.Label>
+              <Form.Control type="file" onChange={onHandleFileChange} />
+            </Form.Group>
+            <Button
+              variant="danger"
+              onClick={sendFileToIPFS}
+              disabled={mintDisable}
+            >
+              Mint
+            </Button>
+            <Form.Group>
+              <Form.Label>{displayMessage}</Form.Label>
+            </Form.Group>
+          </Form>
+        </Col>
+        <Col></Col>
+      </Row>
+    </Container>
+  );
+};
 
 export default Mint;
